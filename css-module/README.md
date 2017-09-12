284061ec93558598b18b0ff61fc39fbf8330940f
yangyafei <342363079@qq.com>
2017-09-12 11:49:35 +0800

    css modules demos

---
+++ css-module/README.md
@@ -0,0 +1,2 @@
+# 来源  阮一峰css-modules
+> webpack\webpack-dev-server\babel-loader  要安装本项目中指定版本，不然会出错
---
+++ css-module/demo04/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './App.css';
+
+export default () => {
+  return (
+    <div className={style.title}>
+      Hello World
+  </div>
+  );
+};
